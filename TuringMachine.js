5a342d945fc84c6170a98c229ded31b9b0cf725e
Andy Li <andy.srs.li@gmail.com>
2015-10-24 18:50:45 -0700

    Add TuringMachine.js, util.js
    • Factored out Turing machine code into TuringMachine.js.
    • Changed transition results from list to object, with
    .state, .symbol, and .move.
    • Changed move type from boolean to MoveHead and MoveTape,
    which allow both notational conventions.
    
    • Rewrote the tape visualization to use the new files.

---
+++ TuringMachine.js
@@ -0,0 +1,76 @@
+'use strict';
+// requires ./util.js
+
+// Allow for both notational conventions of moving the head or moving the tape
+// FIXME: display L/R according to convention, or specify custom showing function
+// var MoveHead = Object.freeze({'left': {}, 'right': {}});
+var MoveHead = Object.freeze({
+  left: {toString: function() { return 'L'; } },
+  right: {toString: function() { return 'R'; } }
+});
+var MoveTape = Object.freeze({left: MoveHead.right, right: MoveHead.left});
+
+function TuringMachine(transition, initialState, tape) {
+  this.transition = transition;
+  this.state = initialState;
+  this.tape = tape;
+}
+
+TuringMachine.prototype.toString = function() {
+  return String(this.state) + '\n' + String(this.tape);
+};
+
+TuringMachine.prototype.step = function() {
+  var action = this.transition(this.state, this.tape.read());
+  this.tape.write(action.symbol);
+  switch (action.move) {
+    case MoveHead.right: this.tape.headRight(); break;
+    case MoveHead.left:  this.tape.headLeft();  break;
+    default: throw new Error('not a valid movement direction: ' + String(action.move));
+  }
+  this.state = action.state;
+};
+
+// 3 action primitives.
+// 'write' is from the basic TM definition; 'move' and 'dir' are for convenience.
+// the returned objects are frozen to prevent accidental corruption of transition functions
+function write(sym, dir, state) {
+  return Object.freeze({symbol: sym, move: dir, state: state});
+}
+
+function move(dir, state) {
+  return Object.freeze({move: dir, state: state});
+}
+
+function skip(dir) {
+  return Object.freeze({move: dir});
+}
+
+// (state, symbol) -> (state, symbol, direction)
+function interpretAction(state, symbol, action) {
+  return {
+    state: coalesce(action.state, state),
+    symbol: coalesce(action.symbol, symbol),
+    move: nonNull(action.move)
+  };
+}
+
+// TODO: add predicate for halting states
+function makeTransition(obj) {
+  return function(s, sym) {
+    var stateObj = obj[s];
+    if (stateObj === null) {
+      throw new Error('the machine has already reached a halting state: ' + String(s));
+    }
+    if (stateObj === undefined) {
+      throw new Error('not a valid state: ' + String(s));
+    }
+    var action = stateObj[sym];
+    // try wildcard if not matched
+    action = (action === undefined) ? stateObj['_'] : action;
+    if (action == null) {
+      throw new Error('no transition is defined from state ' + String(s) + ' for symbol '+ String(sym));
+    }
+    return interpretAction(s, sym, action);
+  };
+}
---
+++ util.js
@@ -0,0 +1,14 @@
+// misc. utilities
+
+// Return the value if it is not null or undefined; otherwise, throw an error.
+function nonNull(value) {
+  if (value == null) {
+    throw new Error('expected a non-null defined value, but got: ' + String(value));
+  }
+  return value;
+}
+
+// Null coalescing: iff the first argument is null or undefined, return the second.
+function coalesce(a, b) {
+  return (a == null) ? b : a;
+}
