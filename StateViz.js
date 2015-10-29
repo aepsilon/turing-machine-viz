76d90166475f3fc6306e35bbdb022f4aa8d7ecf9
Andy Li <andy.srs.li@gmail.com>
2015-10-29 03:47:13 -0700

    Add arrowheads to edges
    Edges now end at a node's perimeter instead of its center.
    
    Node opacity restored to 100%.

--- StateViz.js
+++ StateViz.js
@@ -1,9 +1,51 @@
+
+
+// *** Arrays as vectors ***
+
+// Add vectors.
+// Note: dimensions are not checked. Missing dimensions become NaN.
+function addV(array1, array2) {
+  return array1.map(function(x, i) { return x + array2[i]; });
+}
+
+function negateV(array) {
+  return array.map(function(x) { return -x; });
+}
+
+function subtractV(array1, array2) {
+  return addV(array1, negateV(array2));
+}
+
+// Scale the vector by a scalar.
+function multiplyV(array, scalar) {
+  return array.map(function(x) { return scalar*x; });
+}
+
+// Vector norm, squared
+function normSqV(array) {
+  function sq(x) { return x*x; }
+  function add(x, y) { return x + y; }
+  return array.map(sq).reduce(add, 0)
+}
+
+// Vector norm
+function normV(array) { return Math.sqrt(normSqV(array)); }
+
+// Return a copy of the vector rescaled as a unit vector (norm = 1).
+function unitV(array) {
+  var n = normV(array);
+  return array.map(function(x) { return x / n; });
+}
+
+// *** D3 diagram ***
+
 function visualizeState(svg, dataset) {
   // based on [Graph with labeled edges](http://bl.ocks.org/jhb/5955887)
   // and [Sticky Force Layout](http://bl.ocks.org/mbostock/3750558)
   var w = 1000;
   var h = 400;
   var linkDistance=200;
+  var nodeRadius = 15;
 
   var colors = d3.scale.category10();
 
@@ -51,9 +93,8 @@ function visualizeState(svg, dataset) {
     .data(dataset.nodes)
     .enter()
     .append("circle")
-    .attr({"r":15,
-           'class': 'node',
-           'fill-opacity':'40%'})
+    .attr({"r": nodeRadius,
+           'class': 'node'})
     .style("fill",function(d,i){return colors(i);})
     // .call(force.drag)
     .each(function(d) { d.domNode = this; })
@@ -83,19 +124,15 @@ function visualizeState(svg, dataset) {
       .attr('xlink:href',function(d,i) {return '#edgepath'+i})
       .text(function(d,i){return d.label});
 
-  var svgdefs = svg.select('defs')
-  svgdefs.append('marker')
+  svg.append('defs').append('marker')
       .attr({'id':'arrowhead',
-             'viewBox':'-0 -5 10 10',
-             'refX':25,
-             'refY':0,
-             //'markerUnits':'strokeWidth',
+             'viewBox':'0 -5 10 10',
+             'refX':10,
              'orient':'auto',
              'markerWidth':10,
-             'markerHeight':10,
-             'xoverflow':'visible'})
-      .append('svg:path')
-          .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+             'markerHeight':10})
+      .append('path')
+          .attr('d', 'M 0 -5 L 10 0 L 0 5 Z')
           .attr('fill', '#ccc')
           .attr('stroke','#ccc');
 
@@ -113,11 +150,15 @@ function visualizeState(svg, dataset) {
               y1 = d.source.y;
           // case: self-loop
           if (d.target.index === d.source.index) {
-            return 'M ' + x1 + ',' + y1 +
-              ' A 30,20 -45 1,1 ' + (x1+1) + ',' + (y1+1);
+            return 'M ' + x1 + ',' + (y1-nodeRadius) +
+              ' A 30,20 -45 1,1 ' + (x1+nodeRadius) + ',' + y1;
           }
           // case: line
-          return 'M '+x1+' '+y1+' L '+ d.target.x +' '+d.target.y;
+          var p1 = [d.source.x, d.source.y];
+          var p2 = [d.target.x, d.target.y];
+          var offset = subtractV(p2, p1);
+          var target = subtractV(p2, multiplyV(unitV(offset), nodeRadius));
+          return 'M '+x1+' '+y1+' L '+ target[0] +' '+ target[1];
       });       
 
       edgelabels.attr('transform',function(d,i){
