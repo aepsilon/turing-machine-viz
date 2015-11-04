ad3e505ed7b885a85df3511c86508055acf9fb8f
Andy Li <andy.srs.li@gmail.com>
2015-11-04 14:32:43 -0800

    Factor out StateViz.css
    Also tidied up StateViz.js.
    
    Attributes that could not be styled with CSS
    (such as dy and startOffset) were left in JS.

--- StateViz.js
+++ StateViz.js
@@ -1,3 +1,4 @@
+'use strict';
 // *** Arrays as vectors ***
 
 // Add vectors.
@@ -23,7 +24,7 @@ function multiplyV(array, scalar) {
 function normSqV(array) {
   function sq(x) { return x*x; }
   function add(x, y) { return x + y; }
-  return array.map(sq).reduce(add, 0)
+  return array.map(sq).reduce(add, 0);
 }
 
 // Vector norm
@@ -69,7 +70,7 @@ function countEdges(edges) {
 // *** D3 diagram ***
 
 // TODO: allow multiple diagrams per page? as is, some element IDs would collide.
-function visualizeState(svg, dataset) {
+function visualizeState(svg, nodeArray, linkArray) {
   // based on [Graph with labeled edges](http://bl.ocks.org/jhb/5955887)
   // and [Sticky Force Layout](http://bl.ocks.org/mbostock/3750558)
   var w = 1000;
@@ -79,12 +80,18 @@ function visualizeState(svg, dataset) {
 
   var colors = d3.scale.category10();
 
+  svg.attr({'width': w,
+            'height': h});
+
+  // Force Layout
+
+  // drag event handlers
   function dragstart(d) {
     d.fixed = true;
     svg.transition()
       .style('box-shadow', 'inset 0 0 1px gold');
   }
-  function dragend(d) {
+  function dragend() {
     svg.transition()
       .style('box-shadow', null);
   }
@@ -92,11 +99,10 @@ function visualizeState(svg, dataset) {
     d.fixed = false;
   }
 
-  svg.attr({"width":w,"height":h});
-
+  // set up force layout
   var force = d3.layout.force()
-      .nodes(dataset.nodes)
-      .links(dataset.edges)
+      .nodes(nodeArray)
+      .links(linkArray)
       .size([w,h])
       .linkDistance([linkDistance])
       .charge([-500])
@@ -104,123 +110,121 @@ function visualizeState(svg, dataset) {
       .gravity(0.05)
       .start();
 
-  var edgepaths = svg.selectAll(".edgepath")
-      .data(dataset.edges)
-      .enter()
-      .append('path')
-      .attr({'class':'edgepath',
-             'stroke':'#ccc',
-             'fill':'none',
-             'marker-end': 'url(#arrowhead)',
-             'id':function(d,i) {return 'edgepath'+i}})
-      .style("pointer-events", "none");
-
   var drag = force.drag()
-    .on('dragstart', dragstart)
-    .on('dragend', dragend);
+      .on('dragstart', dragstart)
+      .on('dragend', dragend);
 
-  var nodes = svg.selectAll("circle")
-    .data(dataset.nodes)
-    .enter()
-    .append("circle")
-    .attr({"r": nodeRadius,
-           'class': 'node'})
-    .style("fill",function(d,i){return colors(i);})
-    // .call(force.drag)
-    .each(function(d) { d.domNode = this; })
-    .on('dblclick', releasenode)
-    .call(drag)
+  // Edges
+  var edgeSelection = svg.selectAll('.edgepath')
+    .data(linkArray)
+    .enter();
 
-  var nodelabels = svg.selectAll(".nodelabel") 
-     .data(dataset.nodes)
-     .enter()
-     .append("text")
-     .attr({"class":"nodelabel",
-            "stroke":"black"})
-     .text(function(d){return d.label;});
+  // append edges before nodes, so nodes are painted on top
+  var edgepaths = edgeSelection
+    .append('path')
+      .attr({'class':'edgepath',
+             'id': function(d,i) { return 'edgepath'+i; }});
 
-  var edgelabels = svg.selectAll(".edgelabel")
-      .data(dataset.edges)
-      .enter()
-      .append('text')
-      .attr({'class':'edgelabel',
-             'text-anchor': 'middle',
-             'font-size':10,
-             'dy': -7,
-             'fill':'#aaa'});
+  var edgelabels = edgeSelection
+    .append('text')
+      .attr('class', 'edgelabel')
+      .attr('dy', '-7px');
 
   edgelabels.append('textPath')
-      .attr('xlink:href',function(d,i) {return '#edgepath'+i})
+      .attr('xlink:href', function(d,i) { return '#edgepath'+i; })
       .attr('startOffset', '50%')
-      .text(function(d,i){return d.label});
+      .text(function(d) { return d.label; });
 
-  svg.append('defs').append('marker')
+  // Nodes
+  var nodeSelection = svg.selectAll('.node')
+    .data(nodeArray)
+    .enter();
+
+  var nodecircles = nodeSelection
+    .append('circle')
+      .attr('class', 'node')
+      .attr('r', nodeRadius)
+      .style('fill', function(d,i) { return colors(i); })
+      .each(function(d) { d.domNode = this; })
+      .on('dblclick', releasenode)
+      .call(drag);
+
+  var nodelabels = nodeSelection
+   .append('text')
+     .attr('class', 'nodelabel')
+     .attr('dy', '0.25em') /* dy doesn't work in CSS */
+     .text(function(d) { return d.label; });
+
+  // Arrowhead
+  svg.append('defs')
+    .append('marker')
       .attr({'id':'arrowhead',
              'viewBox':'0 -5 10 10',
              'refX':10,
              'orient':'auto',
              'markerWidth':10,
              'markerHeight':10})
-      .append('path')
-          .attr('d', 'M 0 -5 L 10 0 L 0 5 Z')
-          .attr('fill', '#ccc')
-          .attr('stroke','#ccc');
+    .append('path')
+      .attr('d', 'M 0 -5 L 10 0 L 0 5 Z')
+      .attr('fill', '#ccc')
+      .attr('stroke','#ccc');
 
-  var edgeCount = countEdges(dataset.edges);
+  // Force Layout Update
+  var edgeCount = countEdges(linkArray);
 
-  force.on("tick", function(){
+  force.on('tick', function(){
+    nodecircles.attr({'cx': function(d) { return d.x; },
+                      'cy': function(d) { return d.y; }
+    });
 
-      nodes.attr({"cx":function(d){return d.x;},
-                  "cy":function(d){return d.y;}
-      });
+    nodelabels.attr('x', function(d) { return d.x; }) 
+              .attr('y', function(d) { return d.y; });
 
-      nodelabels.attr("x", function(d) { return d.x; }) 
-                .attr("y", function(d) { return d.y; });
+    edgepaths.attr('d', function(d) {
+      var x1 = d.source.x,
+          y1 = d.source.y;
+      // case: self-loop
+      if (d.target.index === d.source.index) {
+        return 'M ' + x1 + ',' + (y1-nodeRadius) +
+          ' A 30,20 -45 1,1 ' + (x1+nodeRadius) + ',' + y1;
+      }
+      // case: between nodes
+      var p1 = [d.source.x, d.source.y];
+      var p2 = [d.target.x, d.target.y];
+      var offset = subtractV(p2, p1);
+      var target;
+      // TODO: account for multiple edges with same source + target
+      // right now it works for back and forth: one edge out and one edge in.
+      if (edgeCount.numEdgesFromTo(d.target.index, d.source.index)) {
+        // sub-case: arc
+        var radius = 6/5*normV(offset);
+        // Note: SVG's y-axis is flipped, so vector angles are negative
+        // relative to standard coordinates (as used in Math.atan2).
+        // Proof: angle(r <cos ϴ, -sin ϴ>) = angle(r <cos -ϴ, sin -ϴ>) = -ϴ.
+        var angle = angleV(offset);
+        var sep = -Math.PI/2/2; // 90° separation, half on each side
+        var source = addV(p1, vectorFromLengthAngle(nodeRadius, angle+sep));
+        target = addV(p2, vectorFromLengthAngle(nodeRadius, angle+Math.PI-sep));
+        // TODO: consider http://www.w3.org/TR/SVG/paths.html#PathDataCubicBezierCommands
+        return 'M '+source[0]+' '+source[1]+' A '+radius+' '+radius + ' 0 0,1 '+
+          target[0] + ' ' +target[1];
+      } else {
+        // sub-case: straight line
+        target = subtractV(p2, multiplyV(unitV(offset), nodeRadius));
+        return 'M '+x1+' '+y1+' L '+ target[0] +' '+ target[1];
+      }
+    });
 
-      edgepaths.attr('d', function(d) {
-          var x1 = d.source.x,
-              y1 = d.source.y;
-          // case: self-loop
-          if (d.target.index === d.source.index) {
-            return 'M ' + x1 + ',' + (y1-nodeRadius) +
-              ' A 30,20 -45 1,1 ' + (x1+nodeRadius) + ',' + y1;
-          }
-          // case: between nodes
-          var p1 = [d.source.x, d.source.y];
-          var p2 = [d.target.x, d.target.y];
-          var offset = subtractV(p2, p1);
-          // TODO: account for multiple edges with same source + target
-          // right now it works for back and forth: one edge out and one edge in.
-          if (edgeCount.numEdgesFromTo(d.target.index, d.source.index)) {
-            // sub-case: arc
-            var radius = 6/5*normV(offset);
-            // Note: SVG's y-axis is flipped, so vector angles are negative
-            // relative to standard coordinates (as used in Math.atan2).
-            // Proof: angle(r <cos ϴ, -sin ϴ>) = angle(r <cos -ϴ, sin -ϴ>) = -ϴ.
-            var angle = angleV(offset);
-            var sep = -Math.PI/2/2; // 90° separation, half on each side
-            var source = addV(p1, vectorFromLengthAngle(nodeRadius, angle+sep));
-            var target = addV(p2, vectorFromLengthAngle(nodeRadius, angle+Math.PI-sep));
-            return 'M '+source[0]+' '+source[1]+' A '+radius+' '+radius + ' 0 0,1 '+
-              target[0] + ' ' +target[1];
-          } else {
-            // sub-case: straight line
-            var target = subtractV(p2, multiplyV(unitV(offset), nodeRadius));
-            return 'M '+x1+' '+y1+' L '+ target[0] +' '+ target[1];
-          }
-      });
-
-      edgelabels.attr('transform',function(d,i){
-          if (d.target.x<d.source.x){
-              bbox = this.getBBox();
-              rx = bbox.x+bbox.width/2;
-              ry = bbox.y+bbox.height/2;
-              return 'rotate(180 '+rx+' '+ry+')';
-              }
-          else {
-              return 'rotate(0)';
-              }
-      });
-      edgelabels.attr('marker-end', 'url(#arrowhead)');
+    edgelabels.attr('transform', function(d) {
+      // auto-rotate edge labels that are upside-down
+      if (d.target.x < d.source.x) {
+        var box = this.getBBox();
+        var rx = box.x+box.width/2;
+        var ry = box.y+box.height/2;
+        return 'rotate(180 '+rx+' '+ry+')';
+      } else {
+        return null;
+      }
+    });
   });
 }
