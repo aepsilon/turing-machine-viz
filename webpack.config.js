df6193d5b6008e06f48d72dd0d80ee1d46a5ac47
Andy Li <andy.srs.li@gmail.com>
2015-12-03 03:04:43 -0800

    Factor out main.js, improve bundling
    Factored out main app code from index.html to main.js.
    
    Reduced number of webpack bundles.
    Added comment to explain "common chunks" ordering.

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,31 @@ var path = require("path"),
     webpack = require("webpack");
 
 module.exports = {
-  // temporary "entry point", until all of app is transitioned to webpack
+  // split libraries into multiple chunks for more readable compiled code
   entry: {
-    // [] workaround to depend on an entry point (https://github.com/webpack/webpack/issues/300)
     util: ["./util.js"],
-    Position: ["./Position.js"],
-    TapeViz: "./tape/TapeViz.js",
-    StateViz: "./StateViz.js",
     TuringMachine: ["./TuringMachine.js"],
-    NodesLinks: "./NodesLinks.js",
-    ExampleTMs: "./Examples.js"
+    TapeViz: ["./tape/TapeViz.js"],
+    StateViz: ["./StateViz.js"],
+    main: "./main.js"
   },
   output: {
-    library: '[name]',
-    // libraryTarget: 'umd',
     path: path.join(__dirname, "build"),
     filename: "[name].bundle.js"
   },
   externals: {
     "d3": "d3",
     "lodash-fp": "_",
-    "underscore": "underscore"
+    "underscore": "underscore",
+    "./watch.js": "watch"
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      // note: keep "util" last so it contains the webpack bootstrap
-      names: ["Position", "TuringMachine", "util"]
+      // Note on ordering:
+      // Each "commons chunk" takes modules shared with any previous chunks,
+      // including other commons. Later commons therefore contain the fewest dependencies.
+      // For clarity, reverse this to be consistent with browser include order.
+      names: ["util", "TuringMachine", "TapeViz", "StateViz"].reverse()
     })
   ]
 }
