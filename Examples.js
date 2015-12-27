175924a93d5deb8896653e74f3b7aff955925205
Andy Li <andy.srs.li@gmail.com>
2015-12-26 22:32:43 -0800

    Convert examples to string literals
    This is required to show their source code in the editor.

--- Examples.js
+++ Examples.js
@@ -2,14 +2,16 @@
 var TM = require('./TuringMachine.js'),
     Position = require('./Position.js');
 
-var MoveHead = TM.MoveHead, MoveTape = TM.MoveTape,
-    write = TM.write, move = TM.move, skip = TM.skip;
-// convenient synonyms
-var L = MoveHead.left;
-var R = MoveHead.right;
+// var MoveHead = TM.MoveHead, MoveTape = TM.MoveTape,
+//     write = TM.write, move = TM.move, skip = TM.skip;
+// // convenient synonyms
+// var L = MoveHead.left;
+// var R = MoveHead.right;
 
-var ExampleTMs = (function(examples) {
-  // From "Introduction to the Theory of Computation" (3rd ed.) by Michael Sipser, pg. 172
+// {string: string}
+var examples = {};
+
+// From "Introduction to the Theory of Computation" (3rd ed.) by Michael Sipser, pg. 172
 examples.powersOfTwo = `return {
   name: 'powers of two',
   input: '0000',
@@ -18,9 +20,7 @@ examples.powersOfTwo = `return {
   table: (function() {
     var accept = move(R, 'accept');
     var reject = move(R, 'reject');
-
-    // transition function
-    return Object.freeze({
+    return {
       q1: {
         '0': write(' ', R, 'q2'),
         '_': reject
@@ -46,95 +46,91 @@ examples.powersOfTwo = `return {
       },
       'accept': null,
       'reject': null
-    });
+    };
   })()
 };`;
 
-  // Busy beavers, repeat01, and copy1s are from
-  // https://en.wikipedia.org/wiki/Busy_beaver and
-  // https://en.wikipedia.org/wiki/Turing_machine_examples
-  examples.busyBeaver3 = {
-    name: '3-state busy beaver',
-    input: '',
-    blank: 0,
-    startState: 'A',
-    table: (function() {
-      var L = MoveTape.left;
-      var R = MoveTape.right;
-      var halt = move(R, 'halt');
-      return Object.freeze({
-        A: {
-          0: write(1, R, 'B'),
-          1: move(L, 'C')
-        },
-        B: {
-          0: write(1, L, 'A'),
-          1: skip(R)
-        },
-        C: {
-          0: write(1, L, 'B'),
-          1: halt
-        },
-        halt: null
-      });
-    })()
-  };
+// Busy beavers, repeat01, and copy1s are from
+// https://en.wikipedia.org/wiki/Busy_beaver and
+// https://en.wikipedia.org/wiki/Turing_machine_examples
+examples.busyBeaver3 = `return {
+  name: '3-state busy beaver',
+  input: '',
+  blank: 0,
+  startState: 'A',
+  table: (function() {
+    var L = MoveTape.left;
+    var R = MoveTape.right;
+    var halt = move(R, 'halt');
+    return {
+      A: {
+        0: write(1, R, 'B'),
+        1: move(L, 'C')
+      },
+      B: {
+        0: write(1, L, 'A'),
+        1: skip(R)
+      },
+      C: {
+        0: write(1, L, 'B'),
+        1: halt
+      },
+      halt: null
+    };
+  })()
+};`;
 
-  examples.busyBeaver3alt = {
-    name: '3-state busy beaver (alternate)',
-    input: '',
-    blank: 0,
-    startState: 'A',
-    table: (function() {
-      var halt = move(R, 'halt');
-      return Object.freeze({
-        A: {
-          0: write(1, R, 'B'),
-          1: halt
-        },
-        B: {
-          0: move(R, 'C'),
-          1: skip(R)
-        },
-        C: {
-          0: write(1, L, 'C'),
-          1: move(L, 'A')
-        },
-        halt: null
-      });
-    })()
-  };
+examples.busyBeaver3alt = `return {
+  name: '3-state busy beaver (alternate)',
+  input: '',
+  blank: 0,
+  startState: 'A',
+  table: (function() {
+    var halt = move(R, 'halt');
+    return {
+      A: {
+        0: write(1, R, 'B'),
+        1: halt
+      },
+      B: {
+        0: move(R, 'C'),
+        1: skip(R)
+      },
+      C: {
+        0: write(1, L, 'C'),
+        1: move(L, 'A')
+      },
+      halt: null
+    };
+  })()
+};`;
 
-  examples.busyBeaver4 = {
-    name: '4-state busy beaver',
-    input: '',
-    blank: 0,
-    startState: 'A',
-    table: (function() {
-      return Object.freeze({
-        A: {0: write(1, R, 'B'), 1: move(L, 'B')},
-        B: {0: write(1, L, 'A'), 1: write(0, L, 'C')},
-        C: {0: write(1, R, 'H'), 1: move(L, 'D')},
-        D: {0: write(1, R, 'D'), 1: write(0, R, 'A')},
-        H: null
-      });
-    })()
-  };
+examples.busyBeaver4 = `return {
+  name: '4-state busy beaver',
+  input: '',
+  blank: 0,
+  startState: 'A',
+  table: {
+    A: {0: write(1, R, 'B'), 1: move(L, 'B')},
+    B: {0: write(1, L, 'A'), 1: write(0, L, 'C')},
+    C: {0: write(1, R, 'H'), 1: move(L, 'D')},
+    D: {0: write(1, R, 'D'), 1: write(0, R, 'A')},
+    H: null
+  }
+};`;
 
-  examples.repeat01 = {
-    name: 'repeat 0 1',
-    input: '',
-    blank: ' ',
-    startState: 'b',
-    table: (function() {
-      return Object.freeze({
-        b: {' ': write(0, R, 'c')},
-        c: {' ': move(R, 'e')},
-        e: {' ': write(1, R, 'f')},
-        f: {' ': move(R, 'b')}
-      });
-    })()
-  };
+examples.repeat01 = `return {
+  name: 'repeat 0 1',
+  input: '',
+  blank: ' ',
+  startState: 'b',
+  table: {
+    b: {' ': write(0, R, 'c')},
+    c: {' ': move(R, 'e')},
+    e: {' ': write(1, R, 'f')},
+    f: {' ': move(R, 'b')}
+  }
+};`;
 
 examples.copy1s = `return {
   name: 'copy 1s',
@@ -143,7 +139,7 @@ examples.copy1s = `return {
   startState: 's1',
   table: (function() {
     var halt = move(R, 'H');
-    return Object.freeze({
+    return {
       s1: {
         0: halt,
         1: write(0, R, 's2')
@@ -165,33 +161,28 @@ examples.copy1s = `return {
         1: skip(L)
       },
       H: null
-    });
+    };
   })()
 };`;
 
-  examples.binaryIncrement = {
-    name: 'binary increment',
-    input: '1011',
-    blank: ' ',
-    startState: 'right',
-    table: (function() {
-      return Object.freeze({
-        'right': {
-          0: skip(R),
-          1: skip(R),
-          ' ': move(L, 'inc')
-        },
-        'inc': {
-          1: write(0, L, 'inc'),
-          0: write(1, R, 'done'),
-          ' ': write(1, R, 'done')
-        },
-        'done': null
-      });
-    })()
-  };
+examples.binaryIncrement = `return {
+  name: 'binary increment',
+  input: '1011',
+  blank: ' ',
+  startState: 'right',
+  table: {
+    'right': {
+      0: skip(R),
+      1: skip(R),
+      ' ': move(L, 'inc')
+    },
+    'inc': {
+      1: write(0, L, 'inc'),
+      0: write(1, R, 'done'),
+      ' ': write(1, R, 'done')
+    },
+    'done': null
+  }
+};`;
 
-  return examples;
-})(ExampleTMs || {});
-
-module.exports = ExampleTMs;
+module.exports = exports = examples;
