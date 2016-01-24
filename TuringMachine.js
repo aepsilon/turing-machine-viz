5b2936d82ece345af216657a6bd2e22a57541cf4
Andy Li <andy.srs.li@gmail.com>
2016-01-23 20:59:44 -0800

    Simplify TM halting
    • Simplified halting: model undefined transitions as null instead of as
      an exception, and handle it inside TuringMachine itself.
      Also allow a regular halt instead of an error for a state missing an
      instruction for a symbol.
    • Simplified TuringMachine.js to essentials and removed unused old code.

--- TuringMachine.js
+++ TuringMachine.js
@@ -1,86 +1,60 @@
-var util = require('./util.js');
 
-var nonNull = util.nonNull, coalesce = util.coalesce;
-
-// Allow for both notational conventions of moving the head or moving the tape
-// FIXME: display L/R according to convention, or specify custom showing function
-// var MoveHead = Object.freeze({'left': {}, 'right': {}});
-var MoveHead = Object.freeze({
-  left: {toString: function() { return 'L'; } },
-  right: {toString: function() { return 'R'; } }
-});
-var MoveTape = Object.freeze({left: MoveHead.right, right: MoveHead.left});
-
-function TuringMachine(transition, initialState, tape) {
+/**
+ * Construct a Turing machine.
+ * @param {(state, symbol) -> ?{state: state, symbol: symbol, move: direction}}
+ *   transition
+ *   A transition function that, given *only* the current state and symbol,
+ *   returns an object with the following properties: symbol, move, and state.
+ *   Returning null/undefined halts the machine (no transition defined).
+ * @param {state} startState  The state to start in.
+ * @param         tape        The tape to use.
+ */
+function TuringMachine(transition, startState, tape) {
   this.transition = transition;
-  this.state = initialState;
+  this.state = startState;
   this.tape = tape;
 }
 
-TuringMachine.prototype.toString = function() {
+TuringMachine.prototype.toString = function () {
   return String(this.state) + '\n' + String(this.tape);
 };
 
-TuringMachine.prototype.step = function() {
-  var action = this.transition(this.state, this.tape.read());
-  this.tape.write(action.symbol);
-  switch (action.move) {
+/**
+ * Step to the next configuration according to the transition function.
+ * @return {boolean} true if successful (the transition is defined),
+ *   false otherwise (machine halted)
+ */
+TuringMachine.prototype.step = function () {
+  var instruct = this.nextInstruction;
+  if (instruct == null) { return false; }
+  this.tape.write(instruct.symbol);
+  switch (instruct.move) {
     case MoveHead.right: this.tape.headRight(); break;
     case MoveHead.left:  this.tape.headLeft();  break;
-    default: throw new Error('not a valid movement direction: ' + String(action.move));
+    default: throw new Error('not a valid tape movement: ' + String(instruct.move));
   }
-  this.state = action.state;
+  this.state = instruct.state;
+  return true;
 };
 
-// 3 action primitives.
-// 'write' is from the basic TM definition; 'move' and 'dir' are for convenience.
-// the returned objects are frozen to prevent accidental corruption of transition functions
-function write(sym, dir, state) {
-  return Object.freeze({symbol: sym, move: dir, state: state});
-}
+Object.defineProperties(TuringMachine.prototype, {
+  nextInstruction: {
+    get: function () { return this.transition(this.state, this.tape.read()); },
+    enumerable: true
+  },
+  isHalted: {
+    get: function () { return this.nextInstruction == null; },
+    enumerable: true
+  }
+});
 
-function move(dir, state) {
-  return Object.freeze({move: dir, state: state});
-}
-
-function skip(dir) {
-  return Object.freeze({move: dir});
-}
-
-// (state, symbol) -> (state, symbol, direction)
-function interpretAction(state, symbol, action) {
-  return {
-    state: coalesce(action.state, state),
-    symbol: coalesce(action.symbol, symbol),
-    move: nonNull(action.move)
-  };
-}
-
-// TODO: add predicate for halting states
-function makeTransition(obj) {
-  return function(s, sym) {
-    var stateObj = obj[s];
-    if (stateObj === null) {
-      throw new Error('the machine has already reached a halting state: ' + String(s));
-    }
-    if (stateObj === undefined) {
-      throw new Error('not a valid state: ' + String(s));
-    }
-    var action = stateObj[sym];
-    // try wildcard if not matched
-    action = (action === undefined) ? stateObj['_'] : action;
-    if (action == null) {
-      throw new Error('no transition is defined from state ' + String(s) + ' for symbol '+ String(sym));
-    }
-    return interpretAction(s, sym, action);
-  };
-}
+// Allows for both notational conventions of moving the head or moving the tape
+var MoveHead = Object.freeze({
+  left: {toString: function () { return 'L'; } },
+  right: {toString: function () { return 'R'; } }
+});
+var MoveTape = Object.freeze({left: MoveHead.right, right: MoveHead.left});
 
 exports.MoveHead = MoveHead;
 exports.MoveTape = MoveTape;
 exports.TuringMachine = TuringMachine;
-exports.write = write;
-exports.move = move;
-exports.skip = skip;
-exports.interpretAction = interpretAction;
-exports.makeTransition = makeTransition;
