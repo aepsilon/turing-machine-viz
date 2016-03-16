81a66c9ab6f0f06345e0456736c45e4bbe1aa3de
Andy Li <andy.srs.li@gmail.com>
2016-03-16 06:43:19 -0700

    Fix dialog switch breaking scrolling

--- src/sharing/import-panel.js
+++ src/sharing/import-panel.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* global document: false */
 var docimport = require('./import');
 var _ = require('lodash/fp');
 
@@ -10,9 +11,20 @@ function init(args) {
       gistIDForm = args.gistIDForm,
       importArgs = args.importArgs;
 
+  // Attach event handlers
   gistIDForm.addEventListener('submit', function (e) {
     e.preventDefault();
     $dialog.modal('hide');
+    // Workaround needed for opening another modal before a modal is done hiding.
+    // Without this, the <body> scrolls instead of the modal:
+    // modal2.show locks body scroll => modal1.hidden unlocks body scroll
+    // while modal2 is still open.
+    var gistDialogNode = importArgs.dialogNode;
+    $dialog.one('hidden.bs.modal', function () {
+      if (gistDialogNode.classList.contains('in')) {
+        document.body.classList.add('modal-open');
+      }
+    });
 
     var gistID = gistIDForm.querySelector('input[type="text"]').value;
     docimport.importGist(_.assign({gistID: gistID}, importArgs));
